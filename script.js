031246526a963380feec24691d078ed7fd580d1d
bruno velazquez <bb.runito-v@hotmail.com>
2021-07-15 15:04:33 -0300

    proyecto

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+let home = document.querySelector("#home");
+let inicio = document.querySelector("#inicio");
+home.addEventListener("click", scrollInicio, false);
+function scrollInicio(e) {
+  inicio.scrollIntoView({ behavior: "smooth" });
+}
+
+let sobreMi = document.querySelector("#sobreMi");
+let medio = document.querySelector("#medio");
+sobreMi.addEventListener("click", scrollmedio, false);
+function scrollmedio(e) {
+  medio.scrollIntoView({ behavior: "smooth" });
+  
+}
+
+let contacto = document.querySelector("#contacto");
+let final = document.querySelector("#final");
+contacto.addEventListener("click", scrollfinal, false);
+function scrollfinal(e) {
+final.scrollIntoView({ behavior: "smooth" });
+  
+}
+
+
+let github = document.getElementById("github");
+const configuracion_ventana =
+  "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+github.addEventListener("click", abrirGit);
+function abrirGit() {
+  github = window.open(
+    "https://github.com/brunoevelazquez",
+    "github.com",
+    configuracion_ventana
+  );
+}
+
+
+let correo = document.getElementById("correo");
+correo.addEventListener("click", abrirCorreo);
+function abrirCorreo() {
+  correo = window.open(
+    "https://github.com/brunoevelazquez",
+    "github.com",
+    configuracion_ventana
+  );
+}
+
+
+
+
+
+
